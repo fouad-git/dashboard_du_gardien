e94bc5f042d7bc997b74a6327ff3f1baa0bb34cd
fouad-git <63855286+fouad-git@users.noreply.github.com>
2020-06-26 11:43:39 +0200

    update

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,14 +1,22 @@
 <?php
-$nomserveur = 'localhost';
+
+
+$dsn = "mysql:host=localhost;port=3308;dbname=dashboard_du_gardien";
 $identifiant = 'root';
-$login = '';
+$pw = '';
 $option =  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
+
+
 try{
-    $conn = new PDO("mysql:host=$nomserveur;port=3308;dbname=dashboard_du_gardien",$identifiant, $login,$option);
+
+    $conn = new PDO($dsn,$identifiant, $pw,$option);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 }
+
 catch(PDOException $e){
+
     echo "Erreur : " . $e->getMessage();
-  }
+}
 ?>
