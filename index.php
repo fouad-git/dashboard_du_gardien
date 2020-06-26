9d6e1d95fb0d22f818f93f0c974663cada2ad66f
fouad-git <63855286+fouad-git@users.noreply.github.com>
2020-06-26 11:45:55 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    require_once('connexion_bdd.php');
+require_once('connexion_bdd.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -10,41 +11,54 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Tableau de bord du gardien d'immeuble</h1>
-    <p><a href="edit.php">Ajouter</a></p>
+<p><a href="insertion.php">Ajouter</a></p>
 <table>
     <tr>
-<th>Etage</th>
-<th>Position</th>
-<th>Puissance ampoule</th>
-<th>Marque ampoule</th>
-<th>Date</th>
-    
+    <th>
+    id
+    </th>
+        <th> 
+            Etage
+        </th>
+        <th>
+            Position
+        </th>
+        <th>
+            Puissance_ampoule
+        </th>
+        <th>
+            Marque_ampoule
+        </th>
+       <th>Date_changement
+        <th>
+            Modifier/Supprimer
+        </th>
     </tr>
-
-<?php
-    $requete = 'SELECT id, etage, position, puissance_ampoule, marque_ampoule, date_changement FROM gardien';
-    $sth = $conn->prepare($requete);
-    $sth->execute();
-    $resultat = $sth->fetchALL(PDO::FETCH_ASSOC);
-    $intlDateFormater = new IntlDateFormatter('fr FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
-    foreach($resultat as $item){
-        echo '<tr>';
-        echo '<td>'.$item['id'].'</td>';
-        echo '<td>'.$item['etage'].'</td>';
-        echo '<td>'.$item['puissance_ampoule'].'</td>';
-        echo '<td>'.$item['marque_ampoule'].'</td>';
-        echo '<td>'.$item['date'].'</td>';
-        echo '<td>'.$intlDateFormater->format(strtotime($item['date_changement'])).'</td>';
-        echo '<td><a href="edit.php?edit=1&id='.$item['id'].'">Modifier</a><a href="delete.php?id='.$item['id'].'">Supprimer</a></td>';
-        echo'</tr>';
-    }
-?>
+     
+    <?php
+        $sql = 'SELECT id, etage, position, puissance_ampoule, marque_ampoule, date_changement FROM gardien';
+        $pdostat = $conn->prepare($sql);
+        $pdostat->execute();
+        $result = $pdostat->fetchAll(PDO::FETCH_ASSOC);
+        $intlDateFormater = new IntlDateFormatter('fr FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+        foreach($result as $row){
+            echo '<tr>';
+            echo '<td>'.$row['id'].' '.'</td>';
+            echo '<td>'.$row['etage'].' '.$row['position'].'</td>';
+            echo '<td>'.$row['puissance_ampoule'].' '.$row['marque_ampoule'].' '.'</td>';
+            echo '<td>'.$intlDateFormater->format(strtotime($row['date_changement'])).'</td>';
+            echo '<td><a href="insertion.php?insertion=1&id='.$row['id'].'">Modifier</a><a href="supprime.php?id='.$row['id'].'">Supprimer</a></td>';
+            echo'</tr>';
+        } 
+        
+        
+        ?>
 </table>
-<?php
-if(count($resultat) === 0){
-    echo '<p>Vous avez saisi aucun changement</p>';
-}
-    ?>
+
+        <?php
+        if(count($result) === 0){
+            echo '<p>Aucune saisi</p>';
+        }
+        ?>
 </body>
 </html>
