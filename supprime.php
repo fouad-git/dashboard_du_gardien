0e3a897d06beb99d834b9031118d1ba8ff9ab9c2
fouad-git <63855286+fouad-git@users.noreply.github.com>
2020-06-26 11:45:21 +0200

    suppression ok

---
+++ supprime.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('connexion_bdd.php');
+    if(isset($_GET['id'])){
+        $sql = 'DELETE FROM gardien WHERE id= :id';
+        $pdostat = $conn->prepare($sql);
+        $pdostat->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+        $pdostat->execute();
+        header('Location: index.php');
+    }
+
+?>
